5773ce1cbe3d0e7ed612ff4324d0994c691e3d52
nathancohen4299 <ncohen4299@gmail.com>
2020-04-01 16:36:23 -0400

    Added Pipfile

--- src/objs/frame.py
+++ src/objs/frame.py
@@ -1,45 +1,25 @@
-import math
-from typing import List, Dict
+from .priority import Priority
 
-from .packet import Packet
+from enum import Enum
+
+BYTE_LOC = 4
 
 
 class Frame:
-    def __init__(self, n_expected_packets: int):
-        self.n_expected_packets = n_expected_packets
-        self.size = 0
-        self.data_arr: List[bytes] = [None] * n_expected_packets
+    def __init__(self, data: str):
+        self.data = data
+    #
+    # class Priority(Enum):
+    #     LOW = 0b000
+    #     NORMAL = 0b001
+    #     IMPORTANT = 0b010
+    #     CRITICAL = 0b011
+    #
+    #     @staticmethod
+    #     def determine_priority(data: str):
+    #         priority_byte = ord(data[BYTE_LOC]) >> 1
+    #         return Priority(priority_byte)
 
-    def emplace(self, seq_no: int, data: bytes) -> bool:
-        """
-        :param seq_no: packet number in frame
-        :param data: data contained in packet
-        :return: true if data replaces None, false if position has already been filled
-        """
-        if seq_no >= self.n_expected_packets:
-            raise Exception("Packet # is greater than what was initially expected\nExpected Packets: {}\nPacket #: {}".format(
-                    self.n_expected_packets, seq_no))
-        if self.data_arr[seq_no] is None:
-            self.data_arr[seq_no] = data
-            self.size += 1
-            return True
-        return False
-
-    def is_complete(self) -> bool:
-        """
-        :return: true if frame is filled, false otherwise
-        """
-        return self.size == self.n_expected_packets
-
-    def get_data_as_bytes(self) -> bytes:
-        return b"".join(x for x in self.data_arr)
-
-    def to_dict(self) -> Dict:
-        """
-        :return: dictionary of Frame
-        """
-        return {
-            "n_expected_packets": self.n_expected_packets,
-            "size": self.size,
-            "data": "".join(x for x in self.data_arr)
-        }
+    @property
+    def get_priority(self) -> Priority:
+        return Priority.determine_priority(self.data)
